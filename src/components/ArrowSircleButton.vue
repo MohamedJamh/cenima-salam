e710b2fd6a324b6cdec6310b0c8f4575118343c6
MohamedJamh <jam7.mohamed@gmail.com>
2023-04-01 17:51:48 +0000

    updates

---
+++ src/components/ArrowSircleButton.vue
@@ -0,0 +1,18 @@
+<template >
+    <router-link :to="this.link">
+        <div class="tw-flex tw-items-center tw-space-x-2 tw-fill-gray-500">
+            <svg class="tw-h-8 tw-w-8 tw-rounded-full tw-border tw-p-1 tw-border-white tw-fill-white hover:tw-border-red-600 hover:tw-bg-red-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M10.7 17.707l5.7-5.71 -5.71-5.707L9.27 7.7l4.29 4.293 -4.3 4.29Z"></path>
+            </svg>
+        </div>
+    </router-link>
+</template>
+<script>
+export default {
+    name:'arrow-circle-button',
+    props:['link']
+}
+</script>
+<style >
+    
+</style>
